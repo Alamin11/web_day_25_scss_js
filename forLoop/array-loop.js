62ddaa620c717d19d7103490543d63a4112fa90c
Md. Al amin <alamincsecu@gmail.com>
2023-01-30 00:49:03 +0600

    added break and continue

--- forLoop/array-loop.js
+++ forLoop/array-loop.js
@@ -2,6 +2,19 @@
 //     console.log(i);
 // }
 var numbers = [12, 23, 34, 56, 67, 78, 89];
+// for (var i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+// Break 
 for (var i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+    if (numbers[i] > 50) {
+        break;
+    }
+    // console.log(numbers[i]);
+}
+// continue
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] == 56) {
+        continue;
+    } console.log(numbers[i]);
 }
