7d869d8b80b305d952b71064a82ff34a68e8f2df
Md. Al amin <alamincsecu@gmail.com>
2023-01-29 23:59:40 +0600

    added for loop

---
+++ forLoop/array-loop.js
@@ -0,0 +1,7 @@
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+var numbers = [12, 23, 34, 56, 67, 78, 89];
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
