a69996e91dd5cd3ecb3610f996acf1cb49337c16
Md. Al amin <alamincsecu@gmail.com>
2023-01-30 01:06:17 +0600

    added practice problem 01

---
+++ practiceProblems/problem-01.js
@@ -0,0 +1,7 @@
+var monyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalExpense = applePrice + orangePrice;
+var moneyBack = monyGiven - totalExpense;
+console.log(moneyBack);
---
+++ reverse-loop.js
@@ -0,0 +1,4 @@
+var n = 10;
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
