d6187281c0a591764c992ad2d18a495adb63293b
Md. Al amin <alamincsecu@gmail.com>
2023-01-30 01:14:58 +0600

    added practice problem 2

---
+++ practiceProblems/problem-02.js
@@ -0,0 +1,10 @@
+var math = 75.25;
+var biology = 65.80;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+
+var average = (totalMarks / 5).toFixed(2);
+console.log(average);
